8b25716cef969f5df1eb9c7ef96113ee4f2eee7d
Sean <tuansontruongg@gmail.com>
2022-02-15 23:32:40 +0700

    add resolver

--- server/resolver/resolver.js
+++ server/resolver/resolver.js
@@ -1,31 +1,52 @@
 const books = [{
     id: 1,
     name: "forex",
-    genre: "Adventure"
-}]
+    genre: "Adventure",
+    authorId: 1
+},
+{
+    id: 2,
+    name: "how to be rich",
+    genre: "Knowledge",
+    authorId: 1
+},
+{
+    id: 3,
+    name: "Blockchain 101",
+    genre: "Knowledge",
+    authorId: 3
+}];
+
+const authors = [
+    {
+        id: 1,
+        name: "Nam Cao",
+        age: 127
+    },
+    {
+        id: 2,
+        name: "Ngo Tat To",
+        age: 120
+    },
+    {
+        id: 3,
+        name: "Vu Trong Phung",
+        age: 100
+    }
+]
 const resolvers = {
     // QUERY
     Query: {
         books: () => books,
         book: (parent, args) => books.find(book => book.id == args.id),
-        authors: () => [
-            {
-                id: 1,
-                name: "Nam Cao",
-                age: 127
-            },
-            {
-                id: 2,
-                name: "Ngo Tat To",
-                age: 120
-            },
-            {
-                id: 3,
-                name: "Vu Trong Phung",
-                age: 100
-            }
-        ]
+        authors: () => authors
     },
+    Book: {
+        author: (parent) => authors.find(author => author.id == parent.authorId)
+    },
+    Author: {
+        books: (parent) => books.filter(book => book.authorId == parent.id)
+    }
 }
 
 module.exports = resolvers
